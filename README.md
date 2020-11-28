6ff02a6d14b3e7e4082ddc5458f2312dade14f41
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-11-27 18:42:23 -0800

    testing README

---
+++ README.md
@@ -0,0 +1,10 @@
+# hi 
+## Description: 
+ yes 
+ ## Installation: 
+ no 
+ ## Instructions: 
+ what 
+ ## License: 
+ yu
+ [![forthebadge](https://forthebadge.com/images/badges/powered-by-electricity.svg)](https://forthebadge.com)
--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const inquirer= require ("inquirer");
+const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt {
-type: "input"
-message: "What is the title of your README?"
-name: "title"
+inquirer.prompt(
+    [{
+        type: "input",
+        message: "What is the title of your README?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What would you like to put into the description?",
+        name: "description"
+    },
 
-}
+    {
+        type: "input",
+        message: "What steps are required for installation?",
+        name: "installation"
+    },
 
-// array of questions for user
-const questions = [
+    {
+        type: "input",
+        message: "What are the instructions for use?",
+        name: "instructions"
+    },
 
-];
+    {
+        type: "input",
+        message: "What licence is this under?",
+        name: "license"
+    },
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+    ]).then(function(response){
+        let template = `# ${response.title} \n## Description: \n ${response.description} \n ## Installation: \n ${response.installation} \n ## Instructions: \n ${response.instructions} \n ## License: \n ${response.license}\n ${'[![forthebadge](https://forthebadge.com/images/badges/powered-by-electricity.svg)](https://forthebadge.com)'}`
 
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
-
+        fs.writeFile("README.md", template, function(err){
+          err  ? console.log(err):console.log("Success!")
+        })
+    })
