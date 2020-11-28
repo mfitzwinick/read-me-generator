4c75c6f4249c4604bde55462fe76265a145ffb45
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-11-27 16:40:04 -0800

    create index.js file, install inquirer, and check dependencies

---
+++ index.js
@@ -0,0 +1,27 @@
+const inquirer= require ("inquirer");
+const fs = require("fs");
+
+inquirer.prompt {
+type: "input"
+message: "What is the title of your README?"
+name: "title"
+
+}
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
